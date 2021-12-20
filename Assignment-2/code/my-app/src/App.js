df539afc182f4f56e13eedce0cdf0f15d5a17889
barun000 <barunmishra041@gmail.com>
2021-12-20 19:14:51 +0530

    done

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -9,39 +9,39 @@ function App(props) {
 return(
   <div className="container">
   {/* Start your React code here */}
-  <div className='profileImg_container'>
-    <img className='Avatar' src={employee.profileImg} width="100" height="100"></img>
-  </div>
-  <div className='Name_Container'>
-    <h1>{employee.name}</h1>
-  </div>
-  <div className="profile_info">
-    <div className="loc">
-    <label>
-      Location
-    </label>
-    <strong>
-      {employee.location}
-    </strong>
+    <div className='profileImg_container'>
+      <img className='Avatar' src={employee.profileImg} width="100" height="100"></img>
     </div>
-    <div className="bg">
-    <label>
-      Blood Group
-    </label>
-    <strong>
-      {employee.bloodGroup}
-    </strong>
+    <div className='Name_Container'>
+      <h1>{employee.name}</h1>
     </div>
-    <div className="age">
-    <label>
-      Age
-    </label>
-    <strong>
-      {employee.age}
-    </strong>
+    <div className="profile_info">
+      <div className="loc">
+        <label>
+          Location
+        </label>
+        <strong>
+          {employee.location}
+        </strong>
+      </div>
+      <div className="bg">
+        <label> 
+          Blood Group
+        </label>
+        <strong>
+          {employee.bloodGroup}
+        </strong>
+      </div>
+      <div className="age">
+        <label>
+          Age
+        </label>
+        <strong>
+          {employee.age}
+        </strong>
+      </div>
     </div>
   </div>
-</div>
 )
 }
 
